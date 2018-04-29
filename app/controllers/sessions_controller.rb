d1e87d9d1668a29f775b03b2eaa6b2b300278186
Randall Harris Haynes <harrishaynes@gmail.com>
2018-04-29 15:22:06 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,16 @@
 class SessionsController < ApplicationController
   
   def new
+    redirect_to '/' if logged_in?
   end
   
   def create
+    session[:name] = params[:name]
   end
   
   def destroy
+    session.destroy if logged_in?
+    redirect_to 
   end
   
 end
