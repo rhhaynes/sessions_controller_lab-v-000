fc65a7b4c5463544c4c0863f3c0a1879d6b8c60a
Randall Harris Haynes <harrishaynes@gmail.com>
2018-04-29 16:04:55 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
   end
   
   def create
-    if !!params[:name] && !params[:name].strip.empty? && 
-      session[:name] = params[:name] if !logged_in?
+    if !!params[:name] && !params[:name].strip.empty? && !logged_in?
+      session[:name] = params[:name]
     end
     redirect_to login_path
   end
