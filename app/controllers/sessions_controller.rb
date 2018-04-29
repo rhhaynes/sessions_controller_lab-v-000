fedb132fbdb27bad528fb166471333b0f08980ee
Randall Harris Haynes <harrishaynes@gmail.com>
2018-04-29 16:16:18 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,14 @@ class SessionsController < ApplicationController
   end
   
   def create
-    return redirect_to root_path if logged_in?
-    if !!params[:name] && !params[:name].strip.empty?
+    if logged_in?
+      return redirect_to root_path
+    elsif !!params[:name] && !params[:name].strip.empty?
       session[:name] = params[:name]
+      return redirect_to root_path
+    else
+      redirect_to login_path
     end
-    redirect_to login_path
   end
   
   def destroy
