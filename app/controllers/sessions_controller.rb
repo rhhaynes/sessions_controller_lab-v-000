67a4d0a40469653cb901a13998ea750467525463
Randall Harris Haynes <harrishaynes@gmail.com>
2018-04-29 15:39:34 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   
   def create
     binding.pry
-    if !!params[:name] && !params[:name].gsub(\ \,"").empty?
+    if !!params[:name] && !params[:name].gsub(\ {2,}\,"").empty?
     session[:name] = params[:name]
   end
   
